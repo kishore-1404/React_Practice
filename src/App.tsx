7fee13b04da30ff62c659032a75f39226958b667
kishore <omkishorenariki@gmail.com>
2025-06-08 12:34:29 +0530

    Componens in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from './Header'
+import Card from './Card.tsx'
+import Button from './Button.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header />
+      <Card />
+      <Button />
     </>
   )
 }
---
+++ src/Card.tsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-img" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb" alt="Card Image" />
+            <h2 className="card-title">Card Title</h2>
+            <p>This is a description of the card. It provides some information about the content of the card.</p>
+        </div>
+    );
+}
+
+export default Card;
